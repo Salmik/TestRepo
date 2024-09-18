a5610837862a9cf4833999ca65f3170080c3df56
Zhanibek <janibek3394@mail.ru>
2024-09-19 00:45:46 +0500

    first commit

---
+++ HalykWidget.xcframework/ios-arm64/HalykWidget.framework/Headers/HalykWidget.h
@@ -0,0 +1,16 @@
+//
+//  HalykWidget.h
+//  HalykWidget
+//
+//  Created by Zhanibek Lukpanov on 23.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HalykWidget.
+FOUNDATION_EXPORT double HalykWidgetVersionNumber;
+
+//! Project version string for HalykWidget.
+FOUNDATION_EXPORT const unsigned char HalykWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HalykWidget/PublicHeader.h>
