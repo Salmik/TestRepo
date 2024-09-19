8d55776c2a365c9acaa48cb151b7a95093a0e182
Zhanibek <janibek3394@mail.ru>
2024-09-19 10:57:22 +0500

    minor change

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9.0
+// swift-tools-version: 5.3.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,12 @@ import PackageDescription
 let package = Package(
     name: "HalykWidget",
     platforms: [.iOS(.v13)],
-    products: [.library(name: "HalykWidget", targets: ["HalykWidget"])],
-    dependencies: [],
+    products: [
+        .library(name: "HalykWidget", targets: ["HalykWidget"])
+    ],
+    dependencies: [
+        .package(url: "https://gitlab.com/oz-forensics/oz-liveness-ios.git", .upToNextMajor(from: "8.11.0"))
+    ],
     targets: [
         .binaryTarget(name: "HalykWidget", path: "HalykWidget.xcframework")
     ],
